509522b04a5b54b10557c10d3daa1af71311bdd2
Doums <momoseck8@gmail.com>
2022-09-30 17:48:19 +0200

    Fix not found

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const stripe = require('stripe')(process.env.STRIPE_PRIVATE_API_KEY);
-const cors = require('cors');
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const path = require('path');
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors());
 
-// Request post
-app.post('/api/payment', async (req, res) => {
+// Post request
+app.post('/api/payment', cors(), async (req, res) => {
   let { amount, id } = req.body;
-
-  console.log(req.body);
 
   try {
     const payment = await stripe.paymentIntents.create({
@@ -27,17 +24,13 @@ app.post('/api/payment', async (req, res) => {
     });
 
     console.log('Payment', payment);
-
     res.json({
-      message: 'Payment test successfully created',
+      message: 'Payment successful',
       success: true,
     });
   } catch (error) {
-    console.log('Error creating payment');
-    res.json({
-      message: 'Error',
-      success: false,
-    });
+    console.log('Error');
+    res.json({ message: `Payment failed : ${error}`, success: false });
   }
 });
 
@@ -46,6 +39,6 @@ app.get('/', function (_, res) {
   res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'));
 });
 
-// Create a Server
+// Server configuration
 const port = process.env.PORT || 4000;
-app.listen(port, () => console.log('listening on port ' + port));
+app.listen(port, () => console.log(`listening on port ${port}`));
